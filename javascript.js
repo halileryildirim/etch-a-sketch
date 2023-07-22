867c906776c6c037acd1a4de88619e83633c6b2b
halileryildirim <halileryildirim@gmail.com>
2023-07-22 19:38:21 +0300

    Added grid drawer and prompt function for grid size

--- javascript.js
+++ javascript.js
@@ -1,20 +1,37 @@
-function createDiv() {
-    var boardDiv = document.createElement("div");
-    boardDiv.className = 'new-div';
-    boardDiv.textContent = 'TEST';
-    boardDiv.setAttribute('style', 'color:white; background: black;');
+const container = document.querySelector('.container')
+let gridSize = prompt('Enter Grid Size(3-64):')
 
-    return boardDiv;
-}
-
-function modifyDivs(numOfDivs){
-    myDivs = [];
-    i = 0;
-
-    for (i; i<numOfDivs; i++) {
-        myDivs.push(createDiv());
-        document.querySelector(".container").appendChild(myDivs[i]);
+function gridSizeDecide() {
+    while (gridSize<3 || gridSize>64) {
+        gridSize = prompt('Wrong entry, Enter Grid Size(3-64):');
     }
+    return gridSize;
 }
 
-modifyDivs(9);
+function gridDrawer() {
+    
+function multipleDivs() {
+    squareSize = 960/gridSize;
+    console.log(squareSize)
+    let size = parseInt(squareSize); 
+    for(let i = 0; i<gridSize; i++) {
+        let row = container.appendChild(document.createElement('div'));
+        row.style.width = `${squareSize}px`;
+        row.style.height = "960px";
+        row.setAttribute('style', 'display: flex; flex-direction:row; padding: 0')
+        for(let j = 0; j<gridSize; j++) {
+            const square = document.createElement('div');
+            square.className = 'new-div';
+            square.setAttribute('style', 'outline: 1px solid black; display;flex;')
+            square.style.height = `${size}px`;
+            square.style.width = `${size}px`;
+            row.appendChild(square);
+        }
+    }
+    
+}
+multipleDivs();
+}
+
+gridSizeDecide();
+gridDrawer();
