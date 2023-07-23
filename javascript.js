29a0187d84867f239511373b510d582e2344d85a
halileryildirim <halileryildirim@gmail.com>
2023-07-23 20:24:30 +0300

    Added rainbow color function

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ function gridSizeDecide() {
         gridSize = prompt('Enter Grid Size(3-64):');
     }
         return gridSize;
-}
+};
 
 function gridDrawer(gridNumber) {
     squareSize = 960/gridNumber;
@@ -15,60 +15,50 @@ function gridDrawer(gridNumber) {
         for(let j = 0; j<gridNumber; j++) {
             const square = document.createElement('div');
             square.className = 'new-div';
-            square.setAttribute('style', 'outline: 0.5px solid gray; display;flex;')
+            square.setAttribute('style', 'outline: 0.5px solid gray; display;flex;');
             square.style.height = `${squareSize}px`;
             square.style.width = `${squareSize}px`;
             row.appendChild(square);
             square.addEventListener('mouseover', () => {
                 square.style.backgroundColor = color;
-            })
+            });
 
         }
     }  
-}
+};
 
 function gridRemover() {
     let theCanvas = document.querySelector('.container');
     while(theCanvas.hasChildNodes()) {
         theCanvas.removeChild(theCanvas.firstChild);
     }
-}
+};
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-let color;
-const container = document.querySelector('.container')
-let size = gridSizeDecide();
+let color = 'transparent';
+const container = document.querySelector('.container');
+let size = 16;
 gridDrawer(size);
-
 
 const gridbtn = document.querySelector('.gridBtn');
 gridbtn.addEventListener('click', () => {
     gridRemover();
     size = gridSizeDecide();
     gridDrawer(size);
-})
+});
 
 const blackBtn = document.querySelector('.blackBtn');
-blackBtn.addEventListener('click', () => color='black');
+blackBtn.addEventListener('click', () => window.addEventListener('mouseover', () => color='black'));
 
 const rgbBtn = document.querySelector('.rgbBtn');
-rgbBtn.addEventListener('click', () => color= getRandomColor());
+rgbBtn.addEventListener('click', () => window.addEventListener('mouseover', () => color=`hsl(${Math.random() * 360}, 100%, 50%)`));
 
 const eraser = document.querySelector('.whiteBtn');
-eraser.addEventListener('click', () => color ='transparent');
+eraser.addEventListener('click', () => window.addEventListener('mouseover', () => color='transparent'));
 
 const resetBtn = document.querySelector('.resetBtn');
 resetBtn.addEventListener('click', () => {
     gridRemover();
     gridDrawer(size);
-})
+});
 
 
